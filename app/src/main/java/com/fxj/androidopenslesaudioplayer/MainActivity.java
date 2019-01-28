cfff191d443477979501d22cd9dc1559124c2892
fuxianjin <fuxianjin-hj@huajiao.tv>
2019-01-28 15:57:32 +0800

    添加工程

---
+++ app/src/main/java/com/fxj/androidopenslesaudioplayer/MainActivity.java
@@ -0,0 +1,17 @@
+package com.fxj.androidopenslesaudioplayer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    private Button btn01;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn01 = findViewById(R.id.btn01);
+    }
+}
